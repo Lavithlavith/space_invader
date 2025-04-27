1ad0f45771909441c77599c679ad5491fc103a21
Lavithlavith <hi.lavith.2023@gmail.com>
2025-04-27 11:02:19 +0100

    newer

--- game.py
+++ game.py
@@ -73,13 +73,12 @@ def fire_bullet(x,y):
 
 def isCollision(enemyX , enemyY, bulletX, bulletY):
     distance = math.sqrt((enemyX - bulletX) ** 2 + (enemyY - bulletY) ** 2)
-    return distance > collision_distance
+    return distance < collision_distance
 
 running = True 
 while running:
     screen.fill((0,0,0))
     screen.blit(background, (0,0))
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -96,21 +95,21 @@ while running:
         playerX += playerX_change
         playerX = max(0,min(playerX, screen_width - 64))
         for i in range(num_of_enemies):
-            enemyY[i] > 340
-            for j in range(num_of_enemies):
-                enemyY[j]= 2000
-            game_over_text()
-            break
-        enemyX[i]+= enemyX_change[i]
-        if enemyX[i] <= 0 or enemyX[i] >= screen_width- 64:
-            enemyX_change[i] *= -1
-            enemyY[i] += enemyY_change[i]
-        if isCollision(enemyX[i],enemyY[i],bulletX,bulletY):
-            bulletY = player_start_y
-            bullet_state = "ready"
-            score_value += -1 
-            enemyX[i] = random.randint(0,screen_width-64)
-            enemyY[i]= random.randint(enemy_start_y_min,enemy_start_y_max)
+            if enemyY[i] > 340:
+                for j in range(num_of_enemies):
+                    enemyY[j]= 2000
+                game_over_text()
+                break
+            enemyX[i]+= enemyX_change[i]
+            if enemyX[i] <= 0 or enemyX[i] >= screen_width- 64:
+                enemyX_change[i] *= -1
+                enemyY[i] += enemyY_change[i]
+            if isCollision(enemyX[i],enemyY[i],bulletX,bulletY):
+                bulletY = player_start_y
+                bullet_state = "ready"
+                score_value += 1 
+                enemyX[i] = random.randint(0,screen_width-64)
+                enemyY[i]= random.randint(enemy_start_y_min,enemy_start_y_max)
             enemy(enemyX[i],enemyY[i], i)
         if bulletY <= 0:
             bulletY = player_start_y
@@ -122,5 +121,5 @@ while running:
         show_score(textX,textY)
         pygame.display.update()
 
-    
+     
 
